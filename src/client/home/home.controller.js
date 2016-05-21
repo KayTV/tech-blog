5a2602faa927c316db88a1a5a4f5829ad4fb1e9c
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-05-21 17:33:05 -0600

    auth works

--- src/client/home/home.controller.js
+++ src/client/home/home.controller.js
@@ -1,5 +1,5 @@
 angular.module('app')
-.controller('MovieSearchController', ['$scope', 'httpFactory', 'authFactory', function($scope, httpFactory, authFactory){
+.controller('HomeController', ['$scope', 'httpFactory', 'authFactory', function($scope, httpFactory, authFactory){
   $scope.title = 'BLOG';
 
 }]);
